d37d7d647ad3898904968eecae26f8874201765a
lingbin <lingbin.lb@alibaba-inc.com>
2024-12-23 21:09:50 +0800

    lingbin-temp: temp-commit-at-20241223

--- velox/vector/DecodedVector.h
+++ velox/vector/DecodedVector.h
@@ -191,7 +191,7 @@ class DecodedVector {
 
   /// Return null flag for the top-level row.
   bool isNullAt(vector_size_t idx) const {
-    if (!nulls_) {
+    if (nulls_ == nullptr) {
       return false;
     }
 
