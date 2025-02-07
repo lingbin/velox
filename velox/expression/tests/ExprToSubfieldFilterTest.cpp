aaaf5cf144c068951d9db2fbce411efece5ab501
lingbin <lingbin.lb@alibaba-inc.com>
2025-02-07 12:28:17 +0800

    lingbin-temp: temp-commit-at-20250206 velox/expression

--- velox/expression/tests/ExprToSubfieldFilterTest.cpp
+++ velox/expression/tests/ExprToSubfieldFilterTest.cpp
@@ -119,6 +119,16 @@ TEST_F(ExprToSubfieldFilterTest, eqSubfield) {
   ASSERT_FALSE(bigintRange->testNull());
 }
 
+// 这是为了手动测试 array类型
+TEST_F(ExprToSubfieldFilterTest, eqSubfieldArray) {
+  auto call = parseCallExpr("a[1] = 42", ROW({{"a", ARRAY(BIGINT())}}));
+  Subfield subfield;
+  auto filter =
+      ExprToSubfieldFilterParser::getInstance()->leafCallToSubfieldFilter(
+          *call, subfield, evaluator());
+  ASSERT_FALSE(filter);
+}
+
 TEST_F(ExprToSubfieldFilterTest, neq) {
   auto call = parseCallExpr("a <> 42", ROW({{"a", BIGINT()}}));
   Subfield subfield;
