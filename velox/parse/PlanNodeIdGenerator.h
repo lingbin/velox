397eec7981319e0c06e4be45f16dfca33130fd6d
lingbin <lingbin.lb@alibaba-inc.com>
2025-02-08 17:41:21 +0800

    lingbin-temp: temp-commit-at-20250206 velox/parse

--- velox/parse/PlanNodeIdGenerator.h
+++ velox/parse/PlanNodeIdGenerator.h
@@ -23,18 +23,18 @@ namespace facebook::velox::core {
 /// value.
 class PlanNodeIdGenerator {
  public:
-  explicit PlanNodeIdGenerator(int startId = 0) : nextId_{startId} {}
+  explicit PlanNodeIdGenerator(int32_t startId = 0) : nextId_{startId} {}
 
   PlanNodeId next() {
     return fmt::format("{}", nextId_++);
   }
 
-  void reset(int startId = 0) {
+  void reset(int32_t startId = 0) {
     nextId_ = startId;
   }
 
  private:
-  int nextId_;
+  int32_t nextId_;
 };
 
 } // namespace facebook::velox::core
