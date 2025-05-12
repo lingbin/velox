75cb095c23392d5b755a767a7d2b1a5203d72fc3
lingbin <lingbin.lb@alibaba-inc.com>
2025-05-12 13:49:04 +0800

    lingbin-temp: temp-commit-at-20250512: Timestamp

--- velox/type/Timestamp.h
+++ velox/type/Timestamp.h
@@ -77,16 +77,15 @@ struct TimestampToStringOptions {
 std::string::size_type getMaxStringLength(
     const TimestampToStringOptions& options);
 
-struct Timestamp {
+class Timestamp {
  public:
   static constexpr int64_t kMillisecondsInSecond = 1'000;
   static constexpr int64_t kMicrosecondsInMillisecond = 1'000;
-  static constexpr int64_t kMicrosecondsInSecond =
-      kMicrosecondsInMillisecond * kMillisecondsInSecond;
+  static constexpr int64_t kMicrosecondsInSecond = 1'000'000;
   static constexpr int64_t kNanosecondsInMicrosecond = 1'000;
   static constexpr int64_t kNanosecondsInMillisecond = 1'000'000;
-  static constexpr int64_t kNanosInSecond =
-      kNanosecondsInMillisecond * kMillisecondsInSecond;
+  static constexpr int64_t kNanosInSecond = 1'000'000'000;
+
   // The number of days between the Julian epoch and the Unix epoch.
   static constexpr int64_t kJulianToUnixEpochDays = 2440588LL;
   static constexpr int64_t kSecondsInDay = 86400LL;
@@ -184,7 +183,7 @@ struct Timestamp {
 
   // Keep it in header for getting inlined.
   int64_t toMicros() const {
-    // We use int128_t to make sure the computation does not overflows since
+    // We use int128_t to make sure the computation does not overflow since
     // there are cases such that a negative seconds*1000000 does not fit in
     // int64_t, but seconds*1000000 + nanos does. An example is
     // Timestamp(-9223372036855, 224'192'000).
@@ -311,7 +310,7 @@ struct Timestamp {
     return Timestamp(kMaxSeconds, kMaxNanos);
   }
 
-  /// Our own version of gmtime_r to avoid expensive calls to __tz_convert.
+  /// Our own version of 'gmtime_r' to avoid expensive calls to '__tz_convert'.
   /// This might not be very significant in micro benchmark, but is causing
   /// significant context switching cost in real world queries with higher
   /// concurrency (71% of time is on __tz_convert for some queries).
@@ -319,7 +318,7 @@ struct Timestamp {
   /// Return whether the epoch second can be converted to a valid std::tm.
   static bool epochToCalendarUtc(int64_t seconds, std::tm& out);
 
-  /// Our own version of timegm to avoid expensive calls to __tz_convert.
+  /// Our own version of 'timegm' to avoid expensive calls to __tz_convert.
   ///
   /// This function is guaranteed to give same result as std::timegm when it is
   /// successful.
@@ -339,7 +338,7 @@ struct Timestamp {
     }
   }
 
-  /// Converts a std::tm to a time/date/timestamp string in ISO 8601 format
+  /// Converts a 'std::tm' to a time/date/timestamp string in ISO 8601 format
   /// according to TimestampToStringOptions.
   /// @param startPosition the start position of pre-allocated memory to write
   /// string to.
@@ -491,7 +490,9 @@ struct hash<::facebook::velox::Timestamp> {
   }
 };
 
-std::string to_string(const ::facebook::velox::Timestamp& ts);
+inline std::string to_string(const ::facebook::velox::Timestamp& ts) {
+  return ts.toString();
+}
 
 template <>
 class numeric_limits<facebook::velox::Timestamp> {
